d63d466c1275ec3a69b8eba89cdf1e7d2627c391
jpsdr <jpsdr.psx@free.fr>
2017-06-02 19:38:30 +0200

    Final small adjustment.
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- AutoYUY2/AutoYUY2.cpp
+++ AutoYUY2/AutoYUY2.cpp
@@ -3893,7 +3893,7 @@ AVSValue __cdecl Create_AutoYUY2(AVSValue args, void* user_data, IScriptEnvironm
 
 		if (threads_number==0) env->ThrowError("AutoYUY2: Error with the TheadPool while getting CPU info!");
 
-		if (!poolInterface->AllocateThreads(threads_number,0,0,MaxPhysCores,SetAffinity,sleep,-1))
+		if (!poolInterface->AllocateThreads(threads_number,0,0,MaxPhysCores,SetAffinity,true,-1))
 			env->ThrowError("AutoYUY2: Error with the TheadPool while allocating threadpool!");
 	}
 
